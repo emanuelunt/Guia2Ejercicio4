051bc950c5f04c185418c29ce15332859583a465
Marcelo Cano <mecano@catamarca.edu.ar>
2023-08-17 00:27:12 -0300

    Initial commit

---
+++ src/guia2ejercicio4/Categoria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package guia2ejercicio4;
+
+/**
+ *
+ * @author Monitor_Plano
+ */
+public enum Categoria {
+    
+    COMESTIBLE,
+    LIMPIEZA,
+    PERFUMERIA  // van en mayusculas porque por son constante
+}
---
+++ src/guia2ejercicio4/Guia2Ejercicio4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guia2ejercicio4;
+
+/**
+ *
+ * @author Monitor_Plano
+ */
+public class Guia2Ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new JFramePrincipal().setVisible(true);
+    }
+    
+}
